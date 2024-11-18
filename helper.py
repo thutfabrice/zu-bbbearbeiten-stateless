94da36d96973eebbd508ea9880dfe3bc4c94f589
Fabrice Thut <fabrice.thut@bbbaden.ch>
2024-11-18 09:10:26 +0100

    Add Test for Helper.py and modify code

--- helper.py
+++ helper.py
@@ -1,17 +1,30 @@
+import datetime
 from dataclasses import dataclass
+from sortedcontainers import SortedList
 
-items = []
+items = SortedList(key=lambda x: x.date)
 
 
 @dataclass
 class Item:
     text: str
+    date: datetime
     isCompleted: bool = False
 
+def oneWeekFromToday():
+    today = datetime.datetime.now()
+    oneWeek = datetime.timedelta(weeks=1)
+    return today + oneWeek
 
-def add(text):
-    text = text.replace('b', 'bbb').replace('B', 'Bbb')
-    items.append(Item(text))
+def add(text, date=None):
+    text = text.replace("b", "bbb").replace("B", "Bbb")
+
+    if date is None:
+        date = oneWeekFromToday()
+    else:
+        date = datetime.datetime.strptime(date, "%Y-%m-%d")
+    items.add(Item(text, date))
+
 
 
 def get_all():
