80932596be400d681cf39872eb4a37f4223c4e60
Fabrice Thut <fabrice.thut@bbbaden.ch>
2024-11-25 10:17:42 +0100

    Add framework to do pre-commit hooks Close #7

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import helper
 from flask import Flask, request, Response, render_template, redirect, url_for
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     items = helper.get_all()
-    return render_template('index.html', items=items)
+    return render_template("index.html", items=items)
 
 
-@app.route('/add', methods=["POST"])
+@app.route("/add", methods=["POST"])
 def add():
     text = request.form.get("text")
     helper.add(text)
     return redirect(url_for("index"))
 
 
-@app.route('/update/<int:index>')
+@app.route("/update/<int:index>")
 def update(index):
     helper.update(index)
     return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
